30535be13049d6c4533d55554532bdfe02c84a9b
carlos <carlos8189.mk@gmail.com>
2024-10-17 17:36:12 -0300

    feat: added initial RegisterForm component

---
+++ src/app/_components/register-form/register-form.tsx
@@ -0,0 +1,14 @@
+export const RegisterForm = () => {
+  <form>
+    <label htmlFor="name">Nome</label>
+    <input type="text" id="name" />
+    <label htmlFor="tel">Tel</label>
+    <input type="text" id="tel" />
+    <label htmlFor="email">Email</label>
+    <input type="email" id="email" />
+    <label htmlFor="password">Senha</label>
+    <input type="password" id="password" />
+    <label htmlFor="password2">Repita a senha</label>
+    <input type="password2" id="password2" />
+  </form>;
+};
