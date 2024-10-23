b03bec1136c50b590fbee894b4699992270ef150
carlos <carlos8189.mk@gmail.com>
2024-10-23 14:54:22 -0300

    chore: initial Next setup

--- src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginForm } from "../_components/login-form/login-form";
-
-export default function LoginPage() {
-  return <LoginForm />
-}
--- src/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterForm } from '../_components/register-form/register-form';
-
-export default async function RegisterPage() {
-  return <RegisterForm />;
-}
