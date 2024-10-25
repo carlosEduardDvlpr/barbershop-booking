cc6c0d9ff91eac66366da3fe42bfc01de23b8036
carlos <carlos8189.mk@gmail.com>
2024-10-25 16:16:06 -0300

    feat: added a utility function to check email

---
+++ src/utils/verifyEmail.ts
@@ -0,0 +1,6 @@
+export function verifyEmail(email: string) {
+  const REGEXP_EMAIL = new RegExp(
+    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+  );
+  return REGEXP_EMAIL.test(email);
+}
