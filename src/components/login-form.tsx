d4164d00f35a3418a177a6bdab6b97cd666ab346
carlos <carlos8189.mk@gmail.com>
2024-10-26 17:05:53 -0300

    feat: added LoginForm component

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,59 +1,109 @@
-import Link from "next/link"
-
-import { Button } from "@/components/ui/button"
+'use client';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import React from 'react';
+import axios from 'axios';
+import { LoginResponse } from '@/app/api/login/route';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export function LoginForm() {
+  const router = useRouter();
+  const [error, setError] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const emailInputRef = React.useRef<HTMLInputElement>(null);
+  const passwordInputRef = React.useRef<HTMLInputElement>(null);
+
+  async function handleLoginSubmit(ev: React.FormEvent) {
+    ev.preventDefault();
+
+    setLoading(true);
+    setError('');
+
+    const email = emailInputRef.current?.value;
+    const password = passwordInputRef.current?.value;
+
+    try {
+      const response = await axios.post<LoginResponse>('/api/login', {
+        email,
+        password,
+      });
+      const data = response.data;
+      console.log(data)
+
+      setLoading(false);
+
+      if(data) {
+        router.replace("/")
+      }
+    } catch (error) {
+      if (error instanceof Error) setError(error.message);
+    }
+    setLoading(false);
+  }
+
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
         <CardTitle className="text-2xl">Login</CardTitle>
         <CardDescription>
-          Enter your email below to login to your account
+          Entre com seu email e acesse sua conta.
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
+        <form className="grid gap-4" onSubmit={handleLoginSubmit}>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              ref={emailInputRef}
               id="email"
               type="email"
-              placeholder="m@example.com"
+              placeholder="email@example.com"
               required
             />
           </div>
           <div className="grid gap-2">
             <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
+              <Label htmlFor="password">Senha</Label>
               <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
+                Esqueceu a senha?
               </Link>
             </div>
-            <Input id="password" type="password" required />
+            <Input
+              id="password"
+              ref={passwordInputRef}
+              type="password"
+              required
+            />
           </div>
           <Button type="submit" className="w-full">
+            {loading && <Loader2 className="mr-2 animate-spin" />}
             Login
           </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
-          </Button>
-        </div>
+          {error && (
+            <div>
+              <h1 className="text-lg font-bold text-red-600">Erro no login.</h1>
+              <p className="text-sm text-red-600">Senha ou email inválidos.</p>
+            </div>
+          )}
+        </form>
         <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
+          Não possui uma conta?{' '}
+          <Link href="/register" className="underline">
+            Registre-se
           </Link>
         </div>
       </CardContent>
     </Card>
-  )
+  );
 }
